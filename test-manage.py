6643cddaf02a62a3b0cc50c750ab624875892f47
Cauê Thenório <caue@thenorio.com.br>
2019-06-04 22:46:22 -0300

    Replace make_migrations with a more generic manage.py for the test project

--- test-manage.py
+++ test-manage.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
     from django.core.management import execute_from_command_line
 
-    args = sys.argv + ["makemigrations", "django_fast_cep"]
+    args = sys.argv + ["django_fast_cep"]
     execute_from_command_line(args)
