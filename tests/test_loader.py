0eb06b000ef992a1aaba4c673afaa4c6d6311dbf
Cauê Thenório <caue@thenorio.com.br>
2019-06-04 23:02:19 -0300

    Adds migration to import all ceps

---
+++ tests/test_loader.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+from django_fast_cep.models import Cep
+
+
+class SampleTestCase(TestCase):
+    def test_unit_sample(self):
+        pass
+        # load_base_ceps(Cep)
--- tests/test_sample.py
+++
@@ -1,8 +0,0 @@
-from django.test.utils import (
-    TestCase,
-)
-
-class SampleTestCase(TestCase):
-
-    def test_unit_sample(self):
-        pass
