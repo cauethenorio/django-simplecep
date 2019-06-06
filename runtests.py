e8046406b767b7bc60f66fb2855a9707fa5d06f5
Cauê Thenório <caue@thenorio.com.br>
2019-06-05 23:57:29 -0300

    Test populated db

--- runtests.py
+++ runtests.py
@@ -1,14 +1,23 @@
 import os
 import sys
+from unittest.mock import patch
 
 import django
 from django.conf import settings
 from django.test.utils import get_runner
 
+from tests import utils
 
 if __name__ == "__main__":
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+    os.environ["DJANGO_SETTINGS_MODULE"] = "tests.settings"
     django.setup()
+
+    mock = patch(
+        "simplecep.loader.get_cep_file_path",
+        return_value=utils.get_tests_cep_file_path(),
+    )
+    mock.start()
+    utils.get_cep_file_path_mock = mock
 
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,9 +1,53 @@
+from django.forms.models import model_to_dict
 from django.test import TestCase
 
 from simplecep.models import Cep
-from simplecep.loader import load_base_ceps
 
 
-class SampleTestCase(TestCase):
-    def test_unit_sample(self):
-        load_base_ceps(Cep)
+class LoaderTestCase(TestCase):
+    def test_migration_should_populate_db_with_ceps(self):
+        db_value = [model_to_dict(row) for row in Cep.objects.order_by("cep")]
+        self.assertListEqual(
+            db_value,
+            [
+                {
+                    "address": "Praça da Sé",
+                    "cep": "01001000",
+                    "city": "São Paulo",
+                    "district": "Sé",
+                    "extra": None,
+                    "number": None,
+                    "state": "SP",
+                },
+                {
+                    "address": None,
+                    "cep": "18170000",
+                    "city": "Piedade",
+                    "district": None,
+                    "extra": None,
+                    "number": None,
+                    "state": "SP",
+                },
+                {
+                    "address": "Avenida Presidente Castelo Branco",
+                    "cep": "62880970",
+                    "city": "Horizonte",
+                    "district": "Centro",
+                    "extra": " AC Horizonte",
+                    "number": "4106 ",
+                    "state": "CE",
+                },
+                {
+                    "address": "Rodovia Mábio Gonçalves Palhano",
+                    "cep": "86055991",
+                    "city": "Londrina",
+                    "district": None,
+                    "extra": "CPC Patrimônio Regina",
+                    "number": "s/n",
+                    "state": "PR",
+                },
+            ],
+        )
+
+    def test_amigration_should_populate_db_with_ceps(self):
+        pass
