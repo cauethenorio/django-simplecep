c67cc8abed4ced7ba60bdbdfd0162ff56f51683d
Cauê Thenório <caue@thenorio.com.br>
2019-06-25 22:31:20 -0300

    Adds autocomplete attrs to CEPField widget

--- simplecep/fields.py
+++ simplecep/fields.py
@@ -1,10 +1,72 @@
 import re
 
 from django import forms
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError, ImproperlyConfigured
+from django.urls import reverse
+from django.urls.exceptions import NoReverseMatch
 from django.utils.translation import gettext_lazy as _
 
 from simplecep.models import Cep
+
+
+def with_prefix(field: str) -> str:
+    return "data-simplecep-{}".format(field)
+
+
+class CEPBoundField(forms.BoundField):
+    valid_field_types = ("state", "city", "district", "address")
+
+    @staticmethod
+    def get_getcep_url():
+        try:
+            return reverse("simplecep:get-cep", kwargs={"cep": "00000000"})
+        except NoReverseMatch:
+            raise ImproperlyConfigured(
+                "CEPField autcomplete_fields used but no "
+                "'simplecep:get-cep' view found. Include simplecep "
+                "URLconf in your project urls.py"
+            )
+
+    def validate_and_get_fields(self):
+        fields_keys = self.field.autocomplete_fields.keys()
+        valid_field_types = self.valid_field_types
+
+        invalid_fields = list(fields_keys - valid_field_types)
+        if len(invalid_fields):
+            raise ImproperlyConfigured(
+                "Invalid CEPField autocomplete_fields param field "
+                "type(s): {}. Valid types: {}".format(invalid_fields, valid_field_types)
+            )
+        return self.field.autocomplete_fields
+
+    def get_field_id(self, field_name):
+        try:
+            bound_field = self.form[field_name]
+        except KeyError:
+            raise ImproperlyConfigured(
+                "CEPField autocomplete_fields field not found: '{}'. "
+                "Valid form fields: {}".format(
+                    field_name, list(self.form.fields.keys() - [self.name])
+                )
+            )
+        return bound_field.field.widget.attrs.get("id") or bound_field.auto_id
+
+    def build_widget_attrs(self, attrs, widget=None):
+        attrs = super().build_widget_attrs(attrs, widget)
+
+        if self.field.autocomplete_fields is not None:
+            fields = self.validate_and_get_fields()
+
+            if len(fields.keys()):
+                # tell JS this field is an CEP autocomplete source
+                attrs[with_prefix("autocomplete")] = True
+                attrs[with_prefix("get-cep-url")] = self.get_getcep_url()
+
+                for field_type, field_name in fields.items():
+                    attr_key = with_prefix("{}-field-id").format(field_type)
+                    attrs[attr_key] = self.get_field_id(field_name)
+
+        return attrs
 
 
 class CEPField(forms.CharField):
@@ -13,8 +75,9 @@ class CEPField(forms.CharField):
         "not_found": _("CEP not found"),
     }
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, autocomplete_fields=None, **kwargs):
         super().__init__(*args, **kwargs)
+        self.autocomplete_fields = autocomplete_fields
 
     def validate_format(self, value: str) -> str:
         match = re.match("^(\\d{5})-?(\\d{3})$", value)
@@ -42,10 +105,13 @@ class CEPField(forms.CharField):
         attrs = super().widget_attrs(widget)
 
         # set a max_length if it's not defined
-        attrs["maxlength"] = attrs.get("maxlength", 9)
+        attrs.setdefault("maxlength", 9)
 
         # show numeric keyboard on mobile phones
         # https://css-tricks.com/everything-you-ever-wanted-to-know-about-inputmode/
         attrs["inputmode"] = "decimal"
 
         return attrs
+
+    def get_bound_field(self, form, field_name):
+        return CEPBoundField(form, self, field_name)
--- tests/test_form.py
+++ tests/test_form.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
-from django.forms import Form
+from django.core.exceptions import ImproperlyConfigured
+from django.test import TestCase, override_settings
+from django import forms
 
 from simplecep.fields import CEPField
 
@@ -31,14 +32,84 @@ class CEPFormTestCase(TestCase):
         )
 
     def test_cep_field_maxlength_should_be_9_as_default(self):
-        class SimpleForm(Form):
+        class SimpleForm(forms.Form):
             cep = CEPField()
 
         form = SimpleForm()
-        self.assertIn('maxlength="9"', form.as_p())
+        self.assertIn('maxlength="9"', form["cep"].as_widget())
 
-        class AnotherSimpleForm(Form):
+        class AnotherSimpleForm(forms.Form):
             cep = CEPField(max_length=12)
 
         form = AnotherSimpleForm()
-        self.assertIn('maxlength="12"', form.as_p())
+        self.assertIn('maxlength="12"', form["cep"].as_widget())
+
+    def test_cep_field_should_not_add_autocomplete_attrs_by_default(self):
+        class SimpleForm(forms.Form):
+            cep = CEPField()
+
+        form = SimpleForm()
+        self.assertNotIn('data-simplecep-autocomplete"', form["cep"].as_widget())
+
+    def test_cep_field_autcomplete_should_fail_with_wrong_keys(self):
+        class SimpleForm(forms.Form):
+            cep = CEPField(autocomplete_fields={"states": "state"})
+
+        with self.assertRaisesMessage(
+            ImproperlyConfigured,
+            "Invalid CEPField autocomplete_fields param field type(s): ['states']",
+        ):
+            form = SimpleForm()
+            form["cep"].as_widget()
+
+    def test_cep_field_autcomplete_should_fail_with_wrong_field_name(self):
+        class SimpleForm(forms.Form):
+            cep = CEPField(autocomplete_fields={"state": "eztado"})
+            estado = forms.CharField()
+
+        with self.assertRaisesMessage(
+            ImproperlyConfigured,
+            "CEPField autocomplete_fields field not found: 'eztado'. "
+            "Valid form fields: ['estado']",
+        ):
+            form = SimpleForm()
+            form["cep"].as_widget()
+
+    @override_settings(ROOT_URLCONF="tests.empty_urls")
+    def test_cep_field_autocomplete_should_fail_without_getcep_endpoint(self):
+        class SimpleForm(forms.Form):
+            cep = CEPField(autocomplete_fields={"state": "state"})
+            state = forms.CharField()
+
+        with self.assertRaisesMessage(
+            ImproperlyConfigured,
+            "CEPField autcomplete_fields used but no 'simplecep:get-cep' view found",
+        ):
+            form = SimpleForm()
+            form["cep"].as_widget()
+
+    def test_cep_field_empty_autocomplete_fields_should_create_attrs(self):
+        class SimpleForm(forms.Form):
+            cep = CEPField(autocomplete_fields={"state": "estado"})
+            estado = forms.CharField()
+
+        form = SimpleForm()
+        field_html = form["cep"].as_widget()
+
+        self.assertIn("data-simplecep-autocomplete", field_html)
+        self.assertIn('data-simplecep-state-field-id="id_estado"', field_html)
+
+    def test_cep_field_should_correctly_use_custom_fields_ids(self):
+        custom_id = "my_custom_id"
+
+        class SimpleForm(forms.Form):
+            cep = CEPField(autocomplete_fields={"address": "endereco"})
+            endereco = forms.CharField(widget=forms.TextInput(attrs={"id": custom_id}))
+
+        form = SimpleForm()
+        field_html = form["cep"].as_widget()
+
+        self.assertIn("data-simplecep-autocomplete", field_html)
+        self.assertIn(
+            'data-simplecep-address-field-id="{}"'.format(custom_id), field_html
+        )
