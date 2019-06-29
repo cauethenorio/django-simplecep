2c44869caf8c3b6d917c57e51286df35fce03f5c
Cauê Thenório <caue@thenorio.com.br>
2019-06-29 01:49:39 -0300

    Update frontend code to read new JSON encoded data

---
+++ js/src/fields-finder.test.ts
@@ -0,0 +1,72 @@
+import {querySimplecepAutofillFields} from "./scanner";
+
+describe("querySimplecepAutofillFields", () => {
+    test("should find a single field and extract its data", () => {
+        document.body.innerHTML = `
+        <p>
+            <label for="crazy_field_id">Cep:</label>
+            <input
+                type="text"
+                name="cep"
+                maxlength="9"
+                inputmode="decimal"
+                required=""
+                data-simplecep-autofill="{&quot;baseCepURL&quot;: &quot;/my-cep-route/00000000/&quot;, &quot;dataFields&quot;: [{&quot;selector&quot;: &quot;#my_custom_id&quot;, &quot;type&quot;: &quot;address&quot;}]}"
+                id="crazy_field_id"
+            >
+        </p>
+        <p>
+            <label for="id_estado">Estado:</label>
+            <input type="text" name="estado" required="" id="id_estado">
+        </p>
+    `;
+
+        expect(querySimplecepAutofillFields()).toEqual([
+            {
+                cepField: document.getElementById("crazy_field_id"),
+                baseCepURL: "/my-cep-route/00000000/",
+                dataFields: [{type: "address", selector: "#my_custom_id"}]
+            }
+        ]);
+    });
+
+    test("should find multiple fields and extract their data", () => {
+        document.body.innerHTML = `
+        <p><div>
+            <input
+                type="text"
+                name="cep"
+                maxlength="9"
+                inputmode="decimal"
+                required=""
+                data-simplecep-autofill="{&quot;baseCepURL&quot;: &quot;/my-cep-route/00000000/&quot;, &quot;dataFields&quot;: [{&quot;selector&quot;: &quot;#id_estado&quot;, &quot;type&quot;: &quot;state&quot;}]}"
+                id="first_one"
+            >
+        </div></p>
+        <p><div>
+            <input
+                type="text"
+                data-simplecep-autofill="{&quot;baseCepURL&quot;: &quot;/another-cep-route/00000000/&quot;, &quot;dataFields&quot;: [{&quot;selector&quot;: &quot;#id_statez&quot;, &quot;type&quot;: &quot;state&quot;}, {&quot;selector&quot;: &quot;#id_cityz&quot;, &quot;type&quot;: &quot;city&quot;}, {&quot;selector&quot;: &quot;#id_districtz&quot;, &quot;type&quot;: &quot;district&quot;}, {&quot;selector&quot;: &quot;#id_addressz&quot;, &quot;type&quot;: &quot;address&quot;}]}"
+                id="2nd_field"
+            >
+        </div></p>`;
+
+        expect(querySimplecepAutofillFields()).toEqual([
+            {
+                cepField: document.getElementById("first_one"),
+                baseCepURL: "/my-cep-route/00000000/",
+                dataFields: [{type: "state", selector: "#id_estado"}]
+            },
+            {
+                cepField: document.getElementById("2nd_field"),
+                baseCepURL: "/another-cep-route/00000000/",
+                dataFields: [
+                    {type: "state", selector: "#id_statez"},
+                    {type: "city", selector: "#id_cityz"},
+                    {type: "district", selector: "#id_districtz"},
+                    {type: "address", selector: "#id_addressz"}
+                ]
+            }
+        ]);
+    });
+});
---
+++ js/src/fields-finder.ts
@@ -0,0 +1,27 @@
+const dataFieldTypes = ["state", "city", "district", "address"] as const;
+
+type DataFieldType = typeof dataFieldTypes[number];
+
+export type AutofillFieldDataType = {
+    cepField: HTMLInputElement;
+    baseCepURL: string;
+    dataFields: {type: DataFieldType; selector: string};
+};
+
+export function querySimplecepAutofillFields(): Array<AutofillFieldDataType> {
+    const selector = "[data-simplecep-autofill]";
+    let fields: Array<AutofillFieldDataType> = [];
+
+    document.querySelectorAll(selector).forEach((cepField: HTMLInputElement) => {
+        try {
+            const autoFill: AutofillFieldDataType = JSON.parse(
+                cepField.dataset.simplecepAutofill
+            );
+
+            const {baseCepURL, dataFields} = autoFill;
+            fields.push({cepField, baseCepURL, dataFields});
+        } catch {}
+    });
+
+    return fields;
+}
