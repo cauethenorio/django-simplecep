41ebdd1655b0f4a23f12ccb2bea2b5182a714229
Cauê Thenório <caue@thenorio.com.br>
2019-11-18 20:23:49 -0300

    Remove loaders tests code

--- tests/utils.py
+++ tests/utils.py
@@ -1,11 +1,3 @@
-import os
-
-
-def get_tests_cep_file_path():
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(base_dir, "tests_ceps.txt.zip")
-
-
 TEST_DATA = [
     {
         "address": "Praça da Sé",
