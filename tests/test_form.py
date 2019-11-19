6350eaf1b07e50601985090c802fda84aaf349db
Cauê Thenório <caue@thenorio.com.br>
2019-11-19 20:52:07 -0300

    Test CEPField validation

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,5 +1,6 @@
 import html
 import json
+from unittest.mock import patch
 
 from django.core.exceptions import ImproperlyConfigured
 from django.test import TestCase, override_settings
@@ -12,11 +13,10 @@ def html_decode(html_fragment) -> str:
     return html.escape(json.dumps(html_fragment, sort_keys=True))
 
 
+@patch("simplecep.fields.get_cep_data")
 class CEPFormTestCase(TestCase):
-    def test_cep_field_validation(self):
-        field = CEPField()
-        not_found_message = field.error_messages["not_found"]
-        invalid_format_message = field.error_messages["invalid_format"]
+    def test_cep_field_format_validation(self, *args):
+        invalid_format_message = CEPField.default_error_messages["invalid_format"]
 
         self.assertFieldOutput(
             CEPField,
@@ -32,13 +32,37 @@ class CEPFormTestCase(TestCase):
                 "0000000": [invalid_format_message],
                 "000000-000": [invalid_format_message],
                 "18170-0000": [invalid_format_message],
+            },
+        )
+
+    def test_cep_field_not_found_validation(self, mocked_get_cep_data):
+        not_found_message = CEPField.default_error_messages["not_found"]
+        mocked_get_cep_data.return_value = None
+        self.assertFieldOutput(
+            CEPField,
+            {},
+            {
                 "11111111": [not_found_message],
                 "11111-111": [not_found_message],
                 "12345-123": [not_found_message],
             },
         )
 
-    def test_cep_field_maxlength_should_be_9_as_default(self):
+    def test_cep_field_found_validation(self, mocked_get_cep_data):
+        # The real function will return a CEPAdress instance
+        # but the fields only checks if it's null
+        mocked_get_cep_data.return_value = True
+        self.assertFieldOutput(
+            CEPField,
+            {
+                "11111111": "11111-111",
+                "11111-111": "11111-111",
+                "12345-123": "12345-123",
+            },
+            {},
+        )
+
+    def test_cep_field_maxlength_should_be_9_as_default(self, *args):
         class SimpleForm(forms.Form):
             cep = CEPField()
 
@@ -51,14 +75,14 @@ class CEPFormTestCase(TestCase):
         form = AnotherSimpleForm()
         self.assertIn('maxlength="12"', form["cep"].as_widget())
 
-    def test_cep_field_should_not_add_autofill_attrs_by_default(self):
+    def test_cep_field_should_not_add_autofill_attrs_by_default(self, *args):
         class SimpleForm(forms.Form):
             cep = CEPField()
 
         form = SimpleForm()
         self.assertNotIn('data-simplecep-autofill"', form["cep"].as_widget())
 
-    def test_cep_autofill_should_fail_with_wrong_keys(self):
+    def test_cep_autofill_should_fail_with_wrong_keys(self, *args):
         class SimpleForm(forms.Form):
             cep = CEPField(autofill={"states": "state"})
 
@@ -69,7 +93,7 @@ class CEPFormTestCase(TestCase):
             form = SimpleForm()
             form["cep"].as_widget()
 
-    def test_cep_autofill_should_fail_with_wrong_field_name(self):
+    def test_cep_autofill_should_fail_with_wrong_field_name(self, *args):
         class SimpleForm(forms.Form):
             cep = CEPField(autofill={"state": "eztado"})
             estado = forms.CharField()
@@ -83,7 +107,7 @@ class CEPFormTestCase(TestCase):
             form["cep"].as_widget()
 
     @override_settings(ROOT_URLCONF="tests.empty_urls")
-    def test_cep_field_autofill_should_fail_without_getcep_endpoint(self):
+    def test_cep_field_autofill_should_fail_without_getcep_endpoint(self, *args):
         class SimpleForm(forms.Form):
             cep = CEPField(autofill={"state": "state"})
             state = forms.CharField()
@@ -95,7 +119,7 @@ class CEPFormTestCase(TestCase):
             form = SimpleForm()
             form["cep"].as_widget()
 
-    def test_cep_field_autofill_should_send_baseurl(self):
+    def test_cep_field_autofill_should_send_baseurl(self, *args):
         class SimpleForm(forms.Form):
             cep = CEPField(autofill={"city": "#cidade"})
 
@@ -103,7 +127,7 @@ class CEPFormTestCase(TestCase):
         field_html = form["cep"].as_widget()
         self.assertIn(html.escape('"baseCepURL": "/cep/00000000/"'), field_html)
 
-    def test_cep_field_empty_autofill_should_create_attrs(self):
+    def test_cep_field_empty_autofill_should_create_attrs(self, *args):
         class SimpleForm(forms.Form):
             cep = CEPField(autofill={"district": "bairro_xyz"})
             bairro_xyz = forms.CharField()
@@ -119,7 +143,7 @@ class CEPFormTestCase(TestCase):
             field_html,
         )
 
-    def test_cep_field_should_correctly_use_custom_fields_ids(self):
+    def test_cep_field_should_correctly_use_custom_fields_ids(self, *args):
         custom_id = "my_custom_id"
 
         class SimpleForm(forms.Form):
@@ -134,7 +158,7 @@ class CEPFormTestCase(TestCase):
             html_decode([{"type": "address", "selector": "#" + custom_id}]), field_html
         )
 
-    def test_cep_field_empty_autofill_with_id_should_not_lookup_fields(self):
+    def test_cep_field_empty_autofill_with_id_should_not_lookup_fields(self, *args):
         class SimpleForm(forms.Form):
             cep = CEPField(autofill={"state": "#some_node_id", "district": ".bairro"})
 
@@ -148,10 +172,3 @@ class CEPFormTestCase(TestCase):
         self.assertIn(
             html_decode({"type": "state", "selector": "#some_node_id"}), field_html
         )
-
-    def test_cep_field_should_validate_cep(self):
-        class SimpleForm(forms.Form):
-            cep = CEPField()
-
-        form = SimpleForm({"cep": "123"})
-        form.is_valid()
